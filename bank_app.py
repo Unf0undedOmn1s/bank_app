b62a3a483df775ff4bce0452415132f47b1f795b
Unf0undedOmn1s <148717455+Unf0undedOmn1s@users.noreply.github.com>
2024-03-28 18:42:52 +0200

    Add files via upload

---
+++ bank_app.py
@@ -0,0 +1,18 @@
+#Asking user to write their monthly incomes along with expenses
+income = float(input("Enter a monthly income"))
+transport = float(input("Enter your monthly transport expenses:"))
+home_rent = float(input("Enter your monthly rent:"))
+food_groceries = float(input("Enter your monthly food and groceries expenses:"))
+electricity = float(input("Enter electicity bill:"))
+emergency = float(input("Enter your monthly emergency expenses:"))
+
+#Showing money before expenses
+print("Money before expenses:", income)
+
+#Calculating the total amount 
+total_expenses = transport + food_groceries + home_rent + electricity +  emergency
+
+#Remaining balance 
+balance = income - total_expenses
+print("Your remaining balance is:", balance)
+
